24a56c5784483cb575f1ad6d58820da51350dcb5
Ninjaneer87 - Andrej Forgac <komodoutd@gmail.com>
2026-04-03 09:17:00 +0200

    disable relative and min-h

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <header className={clsx('hero hero--primary')}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <div className={styles.heroRow}>
           <div className="">
