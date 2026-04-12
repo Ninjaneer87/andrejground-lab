07ca23e645bd4e2298c0a9cb60fc60e551b70084
Ninjaneer87 - Andrej Forgac <komodoutd@gmail.com>
2026-04-12 07:26:21 +0200

    try list of focusable selectors

--- packages/lab/src/components/Popover/Popover.tsx
+++ packages/lab/src/components/Popover/Popover.tsx
@@ -95,7 +95,6 @@ const PopoverBase = forwardRef<
     const showDelayRef = useRef<NodeJS.Timeout | null>(null);
     const hideDelayRef = useRef<NodeJS.Timeout | null>(null);
     const fitPlacementRef = useRef<PopoverPlacement | null>(null);
-    const isPointerFocusRef = useRef(false);
 
     const onOpenRef = useRef(onOpen);
     const onCloseRef = useRef(onClose);
@@ -295,16 +294,16 @@ const PopoverBase = forwardRef<
       [handleToggle],
     );
 
-    const onPointerDown = useCallback(() => {
-      isPointerFocusRef.current = true;
-    }, []);
-
     const onTriggerFocusHandler = useCallback(
-      (_e: React.FocusEvent<HTMLDivElement>) => {
-        if (isPointerFocusRef.current) {
-          isPointerFocusRef.current = false;
+      (e: React.FocusEvent<HTMLDivElement>) => {
+        if (
+          !e.target.matches(
+            ':focus-visible, a[href], button, input, select, textarea, details, audio[controls], video[controls]',
+          )
+          // !e.target.matches(':focus-visible')
+        )
           return;
-        }
+        if (e.target !== e.currentTarget) return;
         if (isDisabled) return;
 
         if (openOnFocus) {
@@ -327,7 +326,7 @@ const PopoverBase = forwardRef<
     }, [isDisabled, shouldCloseOnTriggerBlur, handleClose, onTriggerBlurRef]);
 
     const handleMouseEnter = useCallback(() => {
-      if (isDisabled || isPointerFocusRef.current) return;
+      if (isDisabled) return;
 
       if (showDelayRef.current) clearTimeout(showDelayRef.current);
       if (hideDelayRef.current) clearTimeout(hideDelayRef.current);
@@ -548,7 +547,6 @@ const PopoverBase = forwardRef<
       'data-popover-trigger-root-id': rootPopoverId ?? popoverId,
       'data-popover-trigger-current-id': popoverId,
       onKeyDown: onTriggerKeyDown,
-      onPointerDown,
       onFocus: onTriggerFocusHandler,
       onBlur: onTriggerBlurHandler,
       tabIndex: isDisabled ? -1 : 0,
